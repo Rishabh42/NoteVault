1fea947192326239c8451cf8d9786ae56065ded5
rthane <rthane@gitlab.cs.mcgill.ca>
2023-03-30 10:40:50 -0400

    renamed to frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,66 @@
+
+import * as React from 'react';
+import Note from "../Note/Note";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import ButtonBase from '@mui/material/ButtonBase';
+import Grid from "@mui/material/Unstable_Grid2";
+import Typography from "@mui/material/Typography";
+import CreateNote from '../CreateNote/CreateNote';
+import axios from "../../axios";
+
+const NoteListItem = ({ title, lastModified }) => (
+    <Box sx={{ background: "#ddd" }} >
+        <Typography variant='h6'>{title}</Typography>
+        <Typography>{new Date(lastModified).toLocaleString()}</Typography>
+    </Box>
+)
+
+const Home = () => {
+    const [notes, setNotes] = React.useState([]); // {title: string, body: string, lastModified: Date}
+    const [index, setIndex] = React.useState(0);
+    const [mode, setMode] = React.useState(notes.length ? 1 : 0) // 1 - Display note, 0 - Create note
+
+    const addNote = (title, body) => {
+        const note = { title: title, body: body, lastModified: Date.now() };
+        console.log(note)
+        //encryption of note variable
+        setNotes(prevNotes => [...prevNotes, note])
+        setIndex(notes.length);
+        setMode(1);
+    }
+
+    const discardNote = () => setMode(1);
+
+    const updateNote = (title, body) => {
+        const note = notes[index];
+        note.title = title;
+        note.body = body;
+        note.lastModified = Date.now();
+        setNotes([...notes.slice(0, index), note, ...notes.slice(index + 1)])
+    }
+
+    React.useEffect(() => {
+        axios.get('/users/notes').then(res => console.log(res.data));
+    }, [])
+
+    return (
+        <Box sx={{ flexGrow: 1, m: 2 }}>
+            <Grid container columnGap={2}>
+                <Grid xs={4} justifyContent="center" >
+                    <Button variant="contained" onClick={() => setMode(0)} disabled={mode === 0}>Add Note</Button>
+                    {notes.length === 0 ? <Typography>No Notes</Typography> : notes.map((note, i) => (
+                        <ButtonBase sx={{ display: "block", width: "100%", textAlign: "left" }} onClick={() => setIndex(i)}>
+                            <NoteListItem key={i} title={note.title || "Title"} lastModified={note.lastModified} />
+                        </ButtonBase>
+                    ))}
+                </Grid>
+                <Grid xs={7}>
+                    {mode ? <Note initTitle={notes[index].title} initBody={notes[index].body} updateNote={updateNote} /> : <CreateNote addNote={addNote} discardNote={discardNote} />}
+                </Grid>
+            </Grid>
+        </Box>
+    )
+}
+
+export default Home;
