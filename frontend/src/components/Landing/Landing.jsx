26aa1e7abe9c1133c478faf8633937a9f76f4e06
Varun <varunshiri99@gmail.com>
2023-04-04 01:56:48 -0400

    Add guest mode

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Unstable_Grid2';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import LandingImage from "../../assets/images/landing.svg"
 import MetamaskLogo from "../../assets/images/metamask.svg"
@@ -26,10 +27,13 @@ const Landing = () => {
         <Box sx={{ flexGrow: 0 }}>
             <Grid container alignItems="center" justifyContent="center" mt={5}>
                 <Grid xs={12} lg={6} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-                    <Typography variant='h3' textAlign="center" fontWeight="bold">Taking notes.</Typography>
-                    <Typography variant='h3' textAlign="center" fontWeight="bold" color="primary">Privacy redefined.</Typography>
+                    <Typography variant='h3' textAlign="center" fontWeight="bold">Your ideas, your way</Typography>
+                    <Typography variant='h3' textAlign="center" fontWeight="bold" color="primary">We've got your privacy covered.</Typography>
                     <br />
-                    <Button variant='contained' onClick={handleLogin}><img src={MetamaskLogo} height={30} />{" "}Login with Metamask</Button>
+                    <Stack direction="row" spacing={3}>
+                        <Button variant='contained' onClick={handleLogin}><img src={MetamaskLogo} height={30} />{" "}Login with Metamask</Button>
+                        <Button variant='contained' onClick={() => { localStorage.setItem('storage', 'local'); navigate('/home?mode=guest'); }}>Try Guest Mode</Button>
+                    </Stack>
                 </Grid>
                 <Grid xs={12} lg={6} display="flex" justifyContent="center">
                     <img src={LandingImage} width={350} alt="Landing image" />
--- frontend/src/localDb.js
+++ frontend/src/localDb.js
@@ -3,5 +3,6 @@ import Dexie from 'dexie';
 export const db = new Dexie('comp555');
 db.version(1).stores({
     notes: '++_id, note', // Primary key and indexed props
+    keys: '++id'
 });
 
